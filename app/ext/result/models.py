eb5c85c2e1e0ee77e047765bf37bf0b48f9a9c34
Anton Popov <ademaro@ya.ru>
2024-02-22 23:48:31 +0530

    Немного обновлены модели

--- app/ext/result/models.py
+++ app/ext/result/models.py
@@ -1,13 +1,21 @@
+class Tour(db.Model):
+    id: int (pk)
+    title: str(255)
+
 class MatchResult(db.Model):
-    id: int
+    id: int (pk)
+    tour_id: int -> fk Tour.id
     title: str(255)
     result: str(5)
     deadline: datetime
 
 
 class MatchPredict(db.Model):
-    id: int
+    id: int (pk)
     match_id: int -> fk MatchResult.id
     user_id: int -> fk User.id
     predict: str(5)
     dt_predict: datetime
+
+    def getResults():
+        return results
