d90fc9655650ad322d461e557df03d54a364766b
SkoblikovAlexey <Happycow32@yandex.ru>
2024-03-03 05:37:07 +0300

    Добавляет незначительное редактирование заголовков в шаблонах и вью

--- app/ext/prediction/views/__init__.py
+++ app/ext/prediction/views/__init__.py
@@ -5,12 +5,12 @@ prediction = Blueprint("prediction", __name__, template_folder="templates")
 
 @prediction.get("")
 def list():
-    g.breadcrumbs = [{"title": "Список туров"}]
+    g.breadcrumbs = [{"title": "Прогнозы по турам"}]
     tours_list = [1, 2, 3]
     return render_template("prediction/list.j2", tours=tours_list)
 
 
 @prediction.get("/<int:tour_id>")
 def item(tour_id):
-    g.breadcrumbs = [{"controller": ".list", "title": "Список туров"}, {"title": f"Прогноз на {tour_id} тур"}]
+    g.breadcrumbs = [{"controller": ".list", "title": "Прогнозы по турам"}, {"title": f"Прогноз на {tour_id} тур"}]
     return render_template("prediction/item.j2", tour_id=tour_id)
